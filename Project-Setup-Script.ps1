91de188869b99442f545583d406a514738d61dff
Nathaniel Offer <nathanieloffer@users.noreply.github.com>
2020-04-24 16:31:22 +0800

    Initial commit

---
+++ Project-Setup-Script.ps1
@@ -0,0 +1,12 @@
+Write-Host "====================="
+Write-Host "= New GitHub Project="
+Write-Host "====================="
+
+Set-Location -Path C:\Config\Projects -PassThru
+
+$username = Read-Host -Prompt "Enter your GitHub username"
+$projectname = Read-Host -Prompt "Enter a name for the project"
+$projectdescription = Read-Host -Prompt "Enter a description for the project"
+
+New-Item -Name $projectname -ItemType "directory"; Set-Location -Path $projectname
+
